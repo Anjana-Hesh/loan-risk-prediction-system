cc1b2e144ff8448de332e4e8e3f378351f053d8b
Anjana-Heshan <anjana.h@boswingroup.com>
2026-09-07 21:12:30 +0530

    chore: initial monorepo structure with frontend, backend, and ml-service

---
+++ backend/src/main/java/com/loanrisk/loan_risk_backend/LoanRiskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.loanrisk.loan_risk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanRiskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanRiskBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/loanrisk/loan_risk_backend/LoanRiskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loanrisk.loan_risk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanRiskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
